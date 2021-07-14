24ff2e61b511cfbc5beb78a727e6550690a79308
Александр <westerovs@mail.ru>
2021-07-14 22:48:11 +0300

    added second method

---
+++ 2_method/src/js/index.js
@@ -0,0 +1,54 @@
+let position = 0
+const sliderToShow = 4
+const sliderToScroll = 1
+const container = document.querySelector('.slider-container')
+const track = document.querySelector('.slider-track')
+
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
+const items = document.querySelectorAll('.slider-item')
+const itemsCount = items.length
+const itemWidth = container.clientWidth / sliderToShow
+const movePosition = sliderToScroll * itemWidth
+
+items.forEach(item => item.style.minWidth = `${ itemWidth }px`)
+
+btnNext.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + sliderToShow * itemWidth) / itemWidth
+    
+    position -= itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth
+    
+    setPosition()
+    checkBtns()
+})
+
+btnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth
+    
+    position += itemsLeft >= sliderToScroll ? movePosition : itemsLeft * itemWidth
+    
+    setPosition()
+    checkBtns()
+})
+
+const setPosition = () => {
+    track.style.transform = `translateX(${ position }px)`
+}
+
+const checkBtns = () => {
+    btnPrev.disabled = position === 0
+    btnNext.disabled = position <= -(itemsCount - sliderToShow) * itemWidth
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
